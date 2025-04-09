b7b2d231ff9a95790aa9564b7facb26d49c83d00
Domskiii <domalewski.p@wp.pl>
2025-04-09 18:59:36 +0200

    csv uploading and editing products

--- Fridge_app/Controllers/ProductController.cs
+++ Fridge_app/Controllers/ProductController.cs
@@ -1,5 +1,4 @@
-﻿// Controllers/ProductsController.cs
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Fridge_app.Models;
 using Fridge_app.Services;
 using CsvHelper.Configuration;
@@ -8,16 +7,22 @@ using CsvHelper;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using Fridge_app.Models.Mappers;
+using Microsoft.EntityFrameworkCore;
 
-public class ProductsController : Controller
+public class ProductController : Controller
 {
     private readonly ProductService _productService;
 
-    public ProductsController(ProductService productService)
+    public ProductController(ProductService productService)
     {
         _productService = productService;
     }
-
+    public async Task<IActionResult> Index()
+    {
+        var products = await _productService.GetAllProductsAsync();
+        return View(products);
+    }
     public IActionResult Create()
     {
         ViewBag.Units = new List<string> { "pcs", "kg", "liter" };
@@ -37,6 +42,122 @@ public class ProductsController : Controller
         }
         return View(product);
     }
+    public IActionResult Upload()
+    {
+        return View();
+    }
 
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Upload(IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+        {
+            ModelState.AddModelError("", "Proszę wybrać plik.");
+            return View();
+        }
+
+        List<Product> products = new List<Product>();
+        try
+        {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ";",
+                HasHeaderRecord = false,
+                MissingFieldFound = null
+            };
+
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            using (var csv = new CsvReader(reader, config))
+            {
+                csv.Context.RegisterClassMap<ProductCsvMap>();
+                products = csv.GetRecords<Product>().ToList();
+            }
+
+            foreach (var product in products)
+            {
+                product.Unit = "g";
+                product.ProductCategory = ProductCategory.Other;
+                product.PriceMin = 0;
+                product.PriceMax = 0;
+            }
+        }
+        catch (Exception ex)
+        {
+            ModelState.AddModelError("", $"Błąd podczas przetwarzania pliku: {ex.Message}");
+            return View();
+        }
+
+        return View("Preview", products);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Confirm(List<Product> products)
+    {
+        if (ModelState.IsValid)
+        {
+            foreach (var product in products)
+            {
+                await _productService.AddProductAsync(product);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View("Preview", products);
+    }
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var product = await _productService.GetProductAsync(id.Value);
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        ViewBag.Units = new List<string> { "pcs", "kg", "liter", "g" };
+        return View(product);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, Product product)
+    {
+        if (id != product.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                await _productService.UpdateProductAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await ProductExists(product.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        ViewBag.Units = new List<string> { "pcs", "kg", "liter", "g" };
+        return View(product);
+    }
+
+    private async Task<bool> ProductExists(int id)
+    {
+        return (await _productService.GetProductAsync(id)) != null;
+    }
 }
 
---
+++ Fridge_app/Models/Mappers/ProductCsvMap.cs
@@ -0,0 +1,24 @@
+﻿using CsvHelper.Configuration;
+
+namespace Fridge_app.Models.Mappers
+{
+    public sealed class ProductCsvMap : ClassMap<Product>
+    {
+        public ProductCsvMap()
+        {
+            Map(m => m.Name).Index(0);
+            Map(m => m.Kcal).Index(1);
+            Map(m => m.Fat).Index(2);
+            Map(m => m.UnsaturatedFat).Index(3);
+            Map(m => m.Carbohydrates).Index(4);
+            Map(m => m.Sugar).Index(5);
+            Map(m => m.Protein).Index(6);
+            Map(m => m.Fiber).Index(7);
+            Map(m => m.Salt).Index(8);
+            Map(m => m.PriceMin).Ignore();
+            Map(m => m.PriceMax).Ignore();
+            Map(m => m.Unit).Ignore();
+            Map(m => m.ProductCategory).Ignore();
+        }
+    }
+}
