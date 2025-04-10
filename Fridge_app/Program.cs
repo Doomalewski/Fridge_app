1a3f397bcae55309c51c0b5943bc8b4c466e43aa
Domskiii <domalewski.p@wp.pl>
2025-04-10 20:35:24 +0200

    update

--- Fridge_app/Program.cs
+++ Fridge_app/Program.cs
@@ -21,7 +21,13 @@ builder.Services.Configure<RequestLocalizationOptions>(options =>
 builder.Services.AddScoped<ProductService>();
 // Rejestracja generycznego repozytorium
 builder.Services.AddScoped(typeof(IRepository<>), typeof(EfCoreRepository<>));
-
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(options =>
+        {
+            options.LoginPath = "/Account/Login";
+            options.AccessDeniedPath = "/Account/AccessDenied";
+            options.ExpireTimeSpan = TimeSpan.FromDays(30);
+        });
 var app = builder.Build();
 
 
